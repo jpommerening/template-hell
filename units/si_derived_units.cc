1174406a39f4abd2758923d6e6dfbdc3a97995e4
Jonas Pommerening <jonas.pommerening@gmail.com>
2015-01-14 00:37:49 +0100

    Fixes for clang (need to declare name in Unit namespace)

--- units/si_derived_units.cc
+++ units/si_derived_units.cc
@@ -1,26 +1,27 @@
 #include "si_derived_units.h"
 
-namespace SIUnits {
+/* because these are typedefs and the ::name is in the
+ * Unit namespace ... */
+namespace Unit {
 
-  template <> const char * const Hz::name = "Hz";
-  template <> const char * const rad::name = "rad";
-  template <> const char * const sr::name = "sr";
-  template <> const char * const N::name = "N";
-  template <> const char * const Pa::name = "Pa";
-  template <> const char * const J::name = "J";
-  template <> const char * const W::name = "W";
-  template <> const char * const C::name = "C";
-  template <> const char * const F::name = "F";
-  template <> const char * const ohms::name = "";
-  template <> const char * const S::name = "S";
-  template <> const char * const Wb::name = "Wb";
-  template <> const char * const T::name = "T";
-  template <> const char * const H::name = "H";
-  template <> const char * const lm::name = "lm";
-  template <> const char * const lx::name = "lx";
-  template <> const char * const Bq::name = "Bq";
-  template <> const char * const Gy::name = "Gy";
-/*template <> const char * const Sv::name = "Sv";*/
-  template <> const char * const kat::name = "kat";
-
-}
+  template <> const char * const ::SIUnits::Hz::name = "Hz";
+  template <> const char * const ::SIUnits::rad::name = "rad";
+  template <> const char * const ::SIUnits::sr::name = "sr";
+  template <> const char * const ::SIUnits::N::name = "N";
+  template <> const char * const ::SIUnits::Pa::name = "Pa";
+  template <> const char * const ::SIUnits::J::name = "J";
+  template <> const char * const ::SIUnits::W::name = "W";
+  template <> const char * const ::SIUnits::C::name = "C";
+  template <> const char * const ::SIUnits::F::name = "F";
+  template <> const char * const ::SIUnits::ohms::name = "";
+  template <> const char * const ::SIUnits::S::name = "S";
+  template <> const char * const ::SIUnits::Wb::name = "Wb";
+  template <> const char * const ::SIUnits::T::name = "T";
+  template <> const char * const ::SIUnits::H::name = "H";
+  template <> const char * const ::SIUnits::lm::name = "lm";
+  template <> const char * const ::SIUnits::lx::name = "lx";
+  template <> const char * const ::SIUnits::Bq::name = "Bq";
+  template <> const char * const ::SIUnits::Gy::name = "Gy";
+  template <> const char * const ::SIUnits::Sv::name = "Sv";
+  template <> const char * const ::SIUnits::kat::name = "kat";
+};
